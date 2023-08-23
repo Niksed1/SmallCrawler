6c6b024dfb08a188e1e39103a1dfdbb7cf0901dd
Niksed1 <image22061@gmail.com>
2023-08-23 10:27:01 -0700

    added crawling depth

--- crawl.py
+++ crawl.py
@@ -1,5 +1,7 @@
+import re
+import json
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 from collections import defaultdict
@@ -8,6 +10,7 @@ from nltk.corpus import stopwords
 '''strategies to improve: lemmatization, assign weights to the text with more value (headers, bold etc),
 recursion to crawl more pages and collect more information, store inverted index in a database '''
 
+#depth-limited search
 STOP_LIST = set(stopwords.words('english')) #common english stop words
 
 
@@ -16,34 +19,87 @@ class Crawler:
     def __init__(self):
         self._url_seeds = ["https://www.admissions.uci.edu/", "https://catalogue.uci.edu/undergraduatedegrees/", "https://www.ofas.uci.edu/", "https://housing.uci.edu/", "https://food.uci.edu/"]
         self._index = defaultdict(list) #inverted index of word -> url
+        self._links_queue = []
+
+    def depth_limited_crawl(self, base, soup, depth):
+        '''crawling webpages up to the depth of 3
+        Absolute links - links to a page on a separate website (starts with http(s))
+        Relative links - links to a section under the domain name, represented as partial urls (starts with /)
+        Inline links - links that lead to a section within the same page (starts with #)
+        only crawling relative links '''
+
+
+        if depth == 0:
+            return
+        else:
+            #links = soup.find_all('a')
+            #absolute links -> links = [item['href'] for item in soup.select('[href^=http]')]
+            links = soup.find_all('a', href=lambda href: href and not href.startswith(('http', 'https', '#')) and '/' in href)
+            #print(links)
+            for link in links:
+                try:
+                    text_list = [] # holds -> [url, soup]
+                    url = urljoin(base, link['href'])
+
+                    if not self.is_valid_url(url): #checks url validity
+                        break
+
+                    self._links_queue.append(url)
+                    print(f' FETCHED {url} QUEUE SIZE {len(self._links_queue)}')
+                    response = urlopen(url)
+                    new_soup = BeautifulSoup(response, features="lxml")
+                    text_list.append([url, new_soup])
+
+                    for elem in text_list:
+                        soup = elem[1]
+                        token_list = self.tokenize(soup.get_text())
+                        word_dict = self.computeWordFrequencies(token_list)
+                        self.add_data(elem[0], word_dict)
+
+                    self.depth_limited_crawl(base, new_soup, depth-1)
+                except:
+                    print('wrong url format')
+
+    def is_valid_url(self, url):
+        """
+                Returns True or False based on whether the url has to be fetched or not. This is a great place to
+                filter out crawler traps. Duplicated urls will be taken care of by frontier. You don't need to check for duplication
+                in this method
+        """
+        parsed = urlparse(url)
+        if parsed.scheme not in set(["http", "https"]):
+            return False
+        try:
+            return not len(str(url)) > 150 \
+                and not parsed.fragment \
+                and not re.match(".*(share|date|month|year|xml|=).*$", parsed.query.lower()) \
+                and not re.match(".*\.(css|js|bmp|gif|jpe?g|ico" + "|svg|png|tiff?|mid|mp2|mp3|mp4"
+                                 + "|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                                 + "|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso|epub|dll|cnf|tgz|sha1"
+                                 + "|thmx|mso|arff|rtf|jar|csv"
+                                 + "|rm|smil|wmv|swf|wma|zip|rar|gz|pdf)$", parsed.path.lower()) \
+                and not re.match("^.*\/.?[0-9].*\/$", parsed.path.lower())
+
+        except TypeError:
+            print("TypeError for ", parsed)
+            return False
 
     def read_page(self):
         '''requests an html, and uses Beautiful Soup to parse it
         and create a list of extracted texts from the pages.'''
 
-
         try:
             for url in self._url_seeds:
-                text_list = []  # holds -> [url, soup]
-
                 response = urlopen(url)  # response is the html of the page
-                new_soup = BeautifulSoup(response, features="html.parser")
-                # print(soup.get_text())
-                text_list.append([url, new_soup])
-
-                for elem in text_list:
-                    token_list = self.tokenize(elem[1].get_text())
-                    #print(len(token_list))
-                    # print(token_list)
-
-                    word_dict = self.computeWordFrequencies(token_list)
-                    self.add_data(elem[0], word_dict)
+                new_soup = BeautifulSoup(response, features="lxml")
+                #print(new_soup.get_text())
+                self.depth_limited_crawl(url, new_soup, 1)
 
         except Exception as e:
             print("EXCEPTION in read_page:\t", e)
 
 
-        #print(self._index)
+        print(self._links_queue)
         return self._index
 
 
@@ -63,6 +119,8 @@ class Crawler:
                         break
                     else:
                         i+=1
+
+
 
 
     def tokenize(self, text):
@@ -91,8 +149,3 @@ class Crawler:
         for word in tokenList:
             words_dict[word] += 1
         return words_dict
-
-    def is_valid(self):
-        '''function to determine if the url should be fetched.
-        Avoiding calendars, links without information, and traps. For future development'''
-        pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 from crawl import Crawler
+import json
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,14 +15,18 @@ def console_interface(index):
         else:
             for elem in index[inp.lower()]:
                 print(elem)
+def save_to_file(index):
+    print("Started writing dictionary to a file")
+
+    with open("C:/Users/image/Documents/index.txt", "w") as fp:
+        json.dump(index, fp, indent=4)  # encode dict into JSON
+
+    print("Done writing dict into index.txt file")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     crawler = Crawler()
     inverted_index = crawler.read_page()
     console_interface(inverted_index)
+    save_to_file(inverted_index)
 
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
